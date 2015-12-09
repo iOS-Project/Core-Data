45cbaf8b486138017671e688b8861c102419516a
Lun Sovathana <sovathanalun@gmail.com>
2015-12-09 08:24:40 +0700

    Initial Commit

---
+++ Core-Data/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Core-Data
+//
+//  Created by Lun Sovathana on 12/9/15.
+//  Copyright © 2015 Lun Sovathana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Core-Data/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Core-Data
+//
+//  Created by Lun Sovathana on 12/9/15.
+//  Copyright © 2015 Lun Sovathana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
